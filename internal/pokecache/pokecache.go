3cd91fca6fb6558f8558b3164894d9156ef10912
Austin Wiginton <ahwig93@gmail.com>
2024-02-14 00:49:03 -0600

    Add new methods for operating on the cache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -3,28 +3,49 @@ package pokecache
 import (
 	"time"
 	"fmt"
+	"sync"
 )
 
 
 
 type cacheEntry struct {
 	createdAt time.Time
+	value []byte
 }
 
 type Cache struct {
 	cache map[string]cacheEntry
+	mux *sync.Mutex
 }
 
 //New Cache Function
+func NewCache(interval time.Duration) Cache {
+	fmt.Println("create new Cache")
+
+
+}
 
 //Add to Cache Function
+func (c *Cache) add(key string, value []byte) {
+	fmt.Println("Add a new entry to the cache")
+	fmt.Println(key)
+	fmt.Println(value)
+}
 
 //Get from Cache Function
+func (c *Cache) get() {
+	fmt.Println("Get an entry from the cache")
+}
 
 //Loop for reaping stale data
+func (c *Cache) reapLoop(interval time.Duration) {
+	fmt.Println("Start reaping loop for Cache entries to remove data after specific time interval")
+}
 
 //Reap Stale Data
-
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	fmt.Println("Reap stale data entries from the cache")
+}
 
 
 
